dec6e9ae0d0c045ec8472c28c064af651611c72c
Quinn Leavitt <qwe42@live.missouristate.edu>
2023-05-16 19:45:11 -0500

    add-enemy-and-player-death

--- level/Game.gd
+++ level/Game.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+@onready var game_over_screen = $game_over_screen
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,5 +7,9 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+func _on_player_died():
+	await get_tree().create_timer(2).timeout
+	game_over_screen.visible = true
